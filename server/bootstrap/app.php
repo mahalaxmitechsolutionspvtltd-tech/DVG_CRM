d3e6c9cc0874988244f7765c37e47259b66595aa
Mahalaxmi Tech Solutions <mahalaxmitechsolutionspvtltd@gmail.com>
2025-12-25 16:00:59 +0530

    Update app.php

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -13,11 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-
-           
-        ]);
+        $middleware->api(prepend:[]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
